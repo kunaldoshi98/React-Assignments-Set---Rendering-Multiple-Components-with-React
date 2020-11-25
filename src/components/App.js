d5abc4cafade5ad0c4f0a6729516df68b8fab0de
kunaldoshi98 <68844308+kunaldoshi98@users.noreply.github.com>
2020-11-25 20:12:54 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <html data-ns-test="project-name">Gameo</html>
+      <html data-ns-test="project-description">A website to schedule outdoor games and also join an event scheduled for playing any game.</html>
+    </div>
   )
 }
 
